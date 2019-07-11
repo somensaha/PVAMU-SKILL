5f015114f4a699f57a376ee827cad6c824e89bfb
Sumit Rudra <srudra@dccservices.net>
2019-07-11 18:53:52 +0530

    welcome message modified and s3 image link changed

--- functions.js
+++ functions.js
@@ -16,7 +16,7 @@ module.exports = {
     PVAMUStaticTable: 'AskPVAMUStatic',
     PVAMUDynamicTable: 'AskPVAMUDynamic',
     
-    defaultImage: 'https://pvamu-prod.s3.amazonaws.com/pvamu-logo-large.png',
+    defaultImage: 'https://pvamu-prod.s3.amazonaws.com/pvamu-logo-large1.png',
 
     remindersArray: [
         'Thank you for using My Panther, Just a reminder, don\'t forget to ask about P V A M U. ',
